148440a0e3773fcc21b9dd4f601e9e0279edbdc1
wilton <18280840475@163.com>
2016-09-10 15:31:13 +0800

    Add static monitor to it

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var jsSources = [
 ];
 
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function(){
 	gulp.src(coffeeSource)
@@ -42,12 +44,14 @@ gulp.task('compass',function () {
 	.pipe(connect.reload())
 });
 
-gulp.task('default',['coffee','js','compass','connect','watch']);
+gulp.task('default',['html','json','coffee','js','compass','connect','watch']);
 
 gulp.task('watch',function(){
 	gulp.watch(coffeeSource,['coffee']);
 	gulp.watch(jsSources,['js']);
 	gulp.watch('components/sass/*.scss',['compass']);
+	gulp.watch(htmlSources,['html']);
+	gulp.watch(jsonSources,['json']);
 });
 
 gulp.task('connect',function(){
@@ -55,4 +59,14 @@ gulp.task('connect',function(){
 		root: 'builds/development/',
 		livereload: true
 	});
+});
+
+gulp.task('html',function(){
+	gulp.src(htmlSources)
+		.pipe(connect.reload())
+});
+
+gulp.task('json',function(){
+	gulp.src(jsonSources)
+		.pipe(connect.reload())
 });
